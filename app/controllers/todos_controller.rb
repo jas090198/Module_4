44a4b9f7c23f4d98a70872d31a85537cd011d887
jas090198 <jasbir.singh@freshworks.com>
2022-10-03 10:09:38 +0530

    create route added for /todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,16 @@ class TodosController < ApplicationController
         todo = Todo.find(id)
         render plain: todo.to_pleasant_string
     end
+
+    def create
+        text = param[:todo_text]
+        date = DateTime.parse(params[:due_date])
+        new_todo = Todo.create!(
+            todo_text: todo_text,
+            due_date: due_date,
+            completed: false
+        )
+        response_text = "Your new todo is created #{new_todo.id}"
+        render plain: response_text
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "todos", to: "todos#index"
   get "todos/:id", to: "todos#show"
+  post "todos/:todo_text/:due_date", to: "todos#create"
 end
