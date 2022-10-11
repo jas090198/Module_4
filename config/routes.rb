959cb03df909650abd7ee7c8731c517125c4d916
jas090198 <jasbir.singh@freshworks.com>
2022-10-11 13:26:09 +0530

    Added sign up form with bcrypt password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/", to: "todos#index"
+  get "/", to: "home#index"
   resources:todos
+  resources:users
 end
